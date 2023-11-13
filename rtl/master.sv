ec00c50fd67c4454c2e461522c6b0a3b49107b35
RaviduHM99 <munasinghemmrh.19@uom.lk>
2023-11-13 22:47:22 +0530

    master rtl completed
    Master RTL coding completed. Todo - Testing and Simulation

--- rtl/master.sv
+++ rtl/master.sv
@@ -8,7 +8,7 @@ module master(
     output logic M_BSY,
     output logic [7:0] M_DOUT,
     input logic [7:0] M_DIN,
-    input logic [14:0] M_ADDR,
+    input logic [15:0] M_ADDR,
     input logic M_RW,
     input logic M_EXECUTE,
     input logic M_HOLD,
@@ -20,19 +20,31 @@ module master(
     input logic B_GRANT, 
     inout logic B_BUS
 );
-    enum logic [2:0] { IDLE, ADDRESS, ACKN, WRITE, READ, HOLD } state;
+    enum logic [2:0] { IDLE, ADDRESS, ACKNAR, WRITE, ACKNWR, READ, HOLD } state, ackad_state, ackwr_state, rd_state;
 
     localparam WIDTH = 8;
     logic rst, incr;
     logic [WIDTH-1:0] count;
-    counter #(.WIDTH(WIDTH)) counter (.rst(rst), .clk(CLK), .incr(incr), .count(count))
+    counter #(.WIDTH(WIDTH)) counter (.rst(rst), .clk(CLK), .incr(incr), .count(count));
+
+    reg [15:0] REG_ADDRESS;
+    reg [7:0] REG_DATAIN;
+    reg [7:0] REG_DATAOUT;
+
+    assign REG_ADDRESS = M_ADDR;
+    assign REG_DATAIN = M_DIN;
+    assign M_DOUT = REG_DATAOUT;
 
     always_comb begin
-        B_REQ <= (M_HOLD) ? 1'b1 : 1'b0;
-        M_BSY <= (B_GRANT) ? 1'b0 : 1'b1;
+        B_REQ = (M_HOLD) ? 1'b1 : 1'b0;
+        M_BSY = (B_GRANT) ? 1'b0 : 1'b1;
 
-
-
+        unique case (M_RW)
+            1'b0 : ackad_state = (B_ACK) ? READ : ADDRESS;
+            1'b1 : ackad_state = (B_ACK) ? WRITE : ADDRESS;
+        endcase
+        ackwr_state = (B_ACK) ? IDLE : WRITE;
+        rd_state = (B_GRANT) ? READ : HOLD;
     end
 
     always @( posedge CLK or posedge RSTN) begin
@@ -40,15 +52,58 @@ module master(
             state <= IDLE;
             rst <= 1'b0;
             incr <= 1'b0;
+            REG_ADDRESS <= 16'd0;
+            REG_DATAIN <= 8'd0;
+            REG_DATAOUT <= 8'd0;
         end
         else begin
             unique case (state)
                 IDLE : 
-                ADDRESS : 
-                ACKN :
-                WRITE :
-                READ :
-                HOLD :
+
+                ADDRESS : begin
+                    B_RW <= M_RW;
+                    B_BUS <= REG_ADDRESS[15];
+                    incr <= (M_EXECUTE) ? 1'b1 : 1'b0;
+                    REG_ADDRESS <= (M_EXECUTE) ? REG_ADDRESS << 1 : REG_ADDRESS;
+                    state <= (count != 15) ? ADDRESS : ACKNAR;
+                    rst <= (count == 15) ? 1'b1 : 1'b0;
+                end
+
+                ACKNAR : begin
+                    B_RW <= M_RW;
+                    rst <= (count == 3) ? 1'b1 : 1'b0;
+                    state <= (count == 3) ? ackad_state : ACKNAR;
+                    incr <= (M_EXECUTE) ? 1'b1 : 1'b0;
+                end
+
+                WRITE : begin
+                    B_RW <= M_RW;
+                    B_BUS <= REG_DATAIN[7];
+                    REG_DATAIN <= (M_EXECUTE) ? REG_DATAIN << 1 : REG_DATAIN;
+                    rst <= (count == 7) ? 1'b1 : 1'b0;
+                    state <= (count == 7) ? ;
+                    incr <= (M_EXECUTE) ? 1'b1 : 1'b0;
+                end
+
+                ACKNWR : begin
+                    B_RW <= M_RW;
+                    rst <= (count == 3) ? 1'b1 : 1'b0;
+                    state <= (count == 3) ? ackwr_state : ACKNWR;
+                    incr <= (M_EXECUTE) ? 1'b1 : 1'b0;
+                end
+
+                READ : begin
+                    B_RW <= M_RW;
+                    rst <= (count == 7) ? 1'b1 : 1'b0;
+                    state <= (count == 7) ? IDLE : rd_state;
+                    incr <= (M_EXECUTE) ? 1'b1 : 1'b0;
+                    REG_DATAOUT[7] <= B_BUS;
+                    REG_DATAOUT <= (M_EXECUTE) ? REG_DATAOUT >> 1 : REG_DATAOUT;
+                end
+
+                HOLD : begin
+                    state <= (B_GRANT) ? READ : HOLD;
+                end
             endcase
         end
 endmodule
