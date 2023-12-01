f6212a39856b66d59c7df0d876ca7cafb40b8bb8
RaviduHM99 <munasinghemmrh.19@uom.lk>
2023-12-02 00:25:36 +0530

    Master_Completed
    RW bugs....

--- rtl/master.sv
+++ rtl/master.sv
@@ -17,12 +17,13 @@ module master(
     input logic B_ACK,
     output logic B_RW,
     output logic B_REQ,
-    input logic B_GRANT, 
-    inout B_BUS
+    input logic B_GRANT,  //need to add ack when read over to slave to bus
+    input logic B_BUS_IN,
+    output logic B_BUS_OUT
 );
     enum logic [2:0] { IDLE, ADDRESS, ACKNAR, WRITE, ACKNWR, READ, HOLD } state, ackad_state, ackwr_state, rd_state;
 
-    localparam WIDTH = 8;
+    localparam WIDTH = 4;
     logic rst, incr;
     logic [WIDTH-1:0] count;
     counter #(.WIDTH(WIDTH)) counter (.rst(rst), .CLK(CLK), .incr(incr), .count(count));
@@ -31,18 +32,10 @@ module master(
     reg [7:0] REG_DATAIN;
     reg [7:0] REG_DATAOUT;
 
-    assign REG_ADDRESS = M_ADDR;
-    assign REG_DATAIN = M_DIN;
     assign M_DOUT = REG_DATAOUT;
-
-    logic B_BUS_INOUT;
-    logic B_UTIL_CHECK;
-    logic B_BUS_RD;
-
-    assign B_BUS = (B_UTIL_CHECK & M_RW) ? B_BUS_INOUT : 1'bz;
-    assign B_UTIL = B_UTIL_CHECK;
-
-    assign B_BUS_RD = (M_RW) ? 1'bz : B_BUS;
+    
+    logic Index_RD;
+    assign Index_RD = 4'd7 - count;
 
     always_comb begin
         B_REQ = (M_HOLD) ? 1'b1 : 1'b0;
@@ -54,78 +47,68 @@ module master(
         endcase
         ackwr_state = (B_ACK) ? IDLE : WRITE;
         rd_state = (B_GRANT) ? READ : HOLD;
+        B_RW = M_RW;
     end
 
     always @( posedge CLK or posedge RSTN) begin
         if (RSTN) begin
             state <= IDLE;
-            B_RW <= 1'b0;
             rst <= 1'b1;
             incr <= 1'b0;
-            B_UTIL_CHECK <= 1'b0;
-            M_DVALID <= 1'b0;
-            B_BUS_INOUT <= 1'b0;
+            B_UTIL <= 1'b0;
+            //M_DVALID <= 1'b0;
+            REG_ADDRESS <= 16'd0;
+            REG_DATAIN <= 8'd0;
+            REG_DATAOUT <= 8'd0;
+            B_BUS_OUT <= 1'b0;
         end
         else begin
+            B_UTIL <= 1'b0;
+            incr <= (M_EXECUTE & B_GRANT) ? 1'b1 : 1'b0;
+            rst <= 1'b0;
+            //M_DVALID <= 1'b0;
+            REG_ADDRESS <= M_ADDR;
+            REG_DATAIN <= M_DIN;
+            B_BUS_OUT <= 1'b0;
             unique case (state)
                 IDLE : begin
-                    state <= (M_BSY) ? ADDRESS : IDLE;
-                    rst <= 1'b0;
-                    B_UTIL_CHECK <= 1'b0;
-                    incr <= (M_BSY) ? 1'b1 : 1'b0;
+                    state <= (M_EXECUTE & B_GRANT) ? ADDRESS : IDLE;
                 end
 
                 ADDRESS : begin
-                    B_RW <= M_RW;
-                    B_UTIL_CHECK <= 1'b1;
-                    B_BUS_INOUT <= REG_ADDRESS[count];
-                    incr <= 1'b1;
+                    B_UTIL <= 1'b1;
+                    B_BUS_OUT <= REG_ADDRESS[count];
                     state <= (count != 15) ? ADDRESS : ACKNAR;
                     rst <= (count == 14) ? 1'b1 : 1'b0;
-                    M_DVALID <= 1'b0;
                 end
 
                 ACKNAR : begin
-                    B_RW <= M_RW;
-                    B_UTIL_CHECK <= 1'b0;
                     rst <= (count == 2) ? 1'b1 : 1'b0;
                     state <= (count == 3) ? ackad_state : ACKNAR;
-                    incr <= 1'b1;
-                    M_DVALID <= 1'b0;
                 end
 
                 WRITE : begin
-                    B_RW <= M_RW;
-                    B_UTIL_CHECK <= 1'b1;
-                    B_BUS_INOUT <= REG_DATAIN[count];
+                    B_UTIL <= 1'b1;
+                    B_BUS_OUT <= REG_DATAIN[count];
                     rst <= (count == 6) ? 1'b1 : 1'b0;
                     state <= (count != 7) ? WRITE : ACKNWR;
-                    incr <= 1'b1;
-                    M_DVALID <= 1'b0;
                 end
 
                 ACKNWR : begin
-                    B_RW <= M_RW;
-                    B_UTIL_CHECK <= 1'b0;
                     rst <= (count == 2) ? 1'b1 : 1'b0;
                     state <= (count == 3) ? ackwr_state : ACKNWR;
                     M_DVALID <= (count == 3) ? B_ACK : 1'b0;
-                    incr <= 1'b1;
                 end
 
                 READ : begin
-                    B_RW <= M_RW;
-                    B_UTIL_CHECK <= 1'b1;
+                    B_UTIL <= 1'b1;
                     rst <= (count == 6) ? 1'b1 : 1'b0;
                     state <= (count == 7) ? IDLE : rd_state;
                     M_DVALID <= (count == 7) ? 1'b1 : 1'b0;
-                    incr <= 1'b1;
-                    REG_DATAOUT[count] <= B_BUS_RD; ///inverse problem
+                    REG_DATAOUT[Index_RD] <= B_BUS_IN;
                 end
 
                 HOLD : begin
-                    B_UTIL_CHECK <= 1'b0;
-                    B_RW <= M_RW;
                     state <= (B_GRANT) ? READ : HOLD;
                 end
             endcase
--- tb/master_tb.sv
+++ tb/master_tb.sv
@@ -11,7 +11,8 @@ module master_tb;
     logic M_RW, M_EXECUTE, M_HOLD;
 
     logic B_UTIL, B_ACK,B_RW, B_REQ, B_GRANT;
-    reg B_BUS;
+    logic B_BUS_IN;
+    logic B_BUS_OUT;
 
     master dut (.*);
 
@@ -26,9 +27,8 @@ module master_tb;
 
         #(CLK_PERIOD) 
         RSTN <= 0; 
-        /*
-        //////// WRITE TRANSACTION ////////
-        #(CLK_PERIOD) /// 
+/*
+        //////// WRITE TRANSACTION //////// 
         M_DIN <= 8'b10101101;
         M_ADDR <= 16'b1101010101010101;
         M_RW <= 1'b1;
@@ -48,14 +48,15 @@ module master_tb;
         B_ACK <= 1'b0;
         #(CLK_PERIOD*10)
         B_ACK <= 1'b1;
-        #(CLK_PERIOD*2)
-        B_ACK <= 1'b0;
+        #(CLK_PERIOD*1)
         B_GRANT <= 1'b0;
+        #(CLK_PERIOD*1)
+        B_ACK <= 1'b0;
         M_HOLD <= 1'b0;
-        #(CLK_PERIOD*3) */
-
+        M_EXECUTE <= 1'b0;
+        #(CLK_PERIOD*3) 
+*/
         //////// READ TRANSACTION ////////
-        #(CLK_PERIOD) /// 
         M_ADDR <= 16'b1101010101010101;
         M_RW <= 1'b0;
         M_EXECUTE <= 1'b0;
@@ -72,12 +73,27 @@ module master_tb;
         B_ACK <= 1'b1;
         #(CLK_PERIOD*2)
         B_ACK <= 1'b0;
-        B_BUS <= 1'b0;
-        #(CLK_PERIOD*8)
+        B_BUS_IN <= 1'b1;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b0;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b1;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b0;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b1;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b1;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b0;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'b1;
+        #(CLK_PERIOD)
+        B_BUS_IN <= 1'bz;
         B_GRANT <= 1'b0;
         M_HOLD <= 1'b0;
+        M_EXECUTE <= 1'b0;
         #(CLK_PERIOD*3)
-
         $finish();
     end
     
